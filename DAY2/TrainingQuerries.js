c22b94a5675d0530afc53f48f1df965a7b59f928
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-10-28 11:50:13 +0530

    ✨ Working with Geo Spatial Indexes

--- DAY2/TrainingQuerries.js
+++ DAY2/TrainingQuerries.js
@@ -56,7 +56,6 @@ db.users.aggregate([
   },
 ]);
 
-
 // 1.
 db.users.explain("executionStats").find({ "dob.age": { $gt: 70 } });
 //2.
@@ -64,12 +63,90 @@ db.users.createIndex({ "dob.age": 1 });
 // 3.
 db.users.explain("executionStats").find({ "dob.age": { $gt: 70 } });
 
-//1. 
+//1.
 db.users
   .explain("executionStats")
   .find({ "dob.age": { $gt: 60 }, gender: "male" });
 
-  //2. 
+//2.
 db.users.createIndex({ "dob.age": 1, gender: 1 }, { name: "age_gender" });
 // 3.
-db.users.explain("executionStats").find({ "dob.age": { $gt: 60 },"gender":"male" });
+db.users
+  .explain("executionStats")
+  .find({ "dob.age": { $gt: 60 }, gender: "male" });
+
+// Text Indexes
+
+db.products.insertMany([
+  {
+    title: "Laptop",
+    description: "Its is a need for an hour. It also must have a camera.",
+  },
+  {
+    title: "Tv",
+    description: "Its used for entertainment",
+  },
+]);
+
+db.products.createIndex({ description: "text" });
+
+db.products.find(
+  { $text: { $search: "highresolution camera" } },
+  { score: { $meta: "textScore" } },
+);
+
+// Geo Spatial Index
+
+db.places.insertOne({
+  name: "Wankhede Stadium",
+  location: {
+    type: "Point",
+    coordinates: [72.8262038494082, 18.9390354947439],
+  },
+});
+
+db.places.insertOne({
+  name: "INOX",
+  location: {
+    type: "Point",
+    coordinates: [72.82899334675568, 18.943257019601816],
+  },
+});
+
+//1.
+db.places.createIndex({ location: "2dsphere" });
+
+
+//2.
+
+db.places.find({
+  location: {
+    $near: {
+      $geometry: {
+        type: "Point",
+        coordinates: [72.82384350555984, 18.93593017032731],
+      },
+      $minDistance: 10, // meters
+      $maxDistance: 500,
+    },
+  },
+});
+
+
+//3.
+// Provide polygon (collection) of Points[long,lat]
+const point1 = [72.82384350555984, 18.93593017032731]; // marine drive
+const point2 = [72.82770588631921, 18.936457876561306]; // GST Bhavan
+const point3 = [72.82730891939178, 18.941663812387432]; // Liberty Cinema
+const point4 = [72.82403662460638, 18.943774554943527]; // state bank
+
+db.places.find({
+  location: {
+    $geoWithin: {
+      $geometry: {
+        type: "Polygon",
+        coordinates: [[point1, point2, point3, point4, point1]],
+      },
+    },
+  },
+});
