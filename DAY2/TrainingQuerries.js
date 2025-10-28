f74f7579f2c1a4d03e7bbb1583691aadc211a70f
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-10-28 16:32:18 +0530

    ✨ More training querries  (replica sets config)

--- DAY2/TrainingQuerries.js
+++ DAY2/TrainingQuerries.js
@@ -150,3 +150,27 @@ db.places.find({
     },
   },
 });
+
+
+// 3 - instances 
+
+ mongod -> 27017
+ mongod -> 27018
+ mongod -> 27019
+
+
+ // Intiate a replica set
+ rs.initiate({
+    _id:"rs0",
+    members:[
+        {_id:0,host:"localhost:27017"},
+        {_id:1,host:"localhost:27018"},
+        {_id:2,host:"localhost:27019"},
+        
+    ]
+ })
+
+ //status 
+ rs.status()
+
+
